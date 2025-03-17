027a8be4e14b52c07cc8894261fd0596575ef5bb
mjann0712 <mjanngalliguez77@gmail.com>
2025-03-17 12:11:27 +0800

    r2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,33 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import {
-  FaBars,
-  FaTimes,
-} from "react-icons/fa";
-import { motion, AnimatePresence} from "framer-motion";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { motion, AnimatePresence } from "framer-motion";
 import Project from "./sections/project";
 import Hero from "./sections/hero";
 import About from "./sections/about";
 import Experience from "./sections/experience";
 import Contact from "./sections/contact";
 
-
-
 export default function Portfolio() {
   const [scrolling, setScrolling] = useState(false);
- // Handle scroll effect for navbar
- useEffect(() => {
-  const handleScroll = () => {
-    setScrolling(window.scrollY > 50);
-  };
-  window.addEventListener("scroll", handleScroll);
-  return () => window.removeEventListener("scroll", handleScroll);
-}, []);
-
-// Navbar animation variants
-const navVariants = {
-  hidden: { opacity: 0, y: -20 },
-  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
-};
   const [activeSection, setActiveSection] = useState("home");
   const [showNav, setShowNav] = useState(false);
+
+  // Handle scroll effect for navbar
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolling(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  // Navbar animation variants
+  const navVariants = {
+    hidden: { opacity: 0, y: -20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  };
 
   return (
     <div className="bg-gray-900 text-gray-100 min-h-screen font-sans">
@@ -41,39 +37,34 @@ const navVariants = {
         initial="hidden"
         animate="visible"
         className={`hidden sm:fixed top-4 left-1/2 transform -translate-x-1/2 w-[90%] lg:w-[60%] 
-                bg-gray-800 bg-opacity-70 backdrop-blur-lg text-white py-3 px-8 
-                lg:flex justify-center items-center z-50 shadow-lg rounded-full 
-                border border-gray-700 transition-all duration-500 ${
-                  scrolling ? "bg-opacity-90 backdrop-blur-md shadow-orange-400/50" : ""
-                }`}
+                bg-gray-800 bg-opacity-70 backdrop-blur-lg text-white py-4 px-6 
+                lg:flex justify-between items-center z-50 shadow-lg rounded-full 
+                border border-gray-700 transition-all duration-500 ${scrolling ? "bg-opacity-90 backdrop-blur-md shadow-orange-400/50" : ""}`}
       >
-        <div className="hidden lg:flex space-x-1 md:space-x-5">
-          {["home", "about", "projects", "experiences", "contact"].map(
-            (section) => (
+        <div className="space-x-5 flex items-center justify-center">
+          <div className="text-xl font-semibold text-orange-400">My Portfolio</div>
+          <div className="hidden lg:flex space-x-6 ml-10">
+            {["home", "about", "projects", "experiences", "contact"].map((section) => (
               <motion.a
                 key={section}
                 href={`#${section}`}
                 onClick={(e) => {
                   e.preventDefault();
-                  document
-                    .getElementById(section)
-                    ?.scrollIntoView({ behavior: "smooth" });
+                  document.getElementById(section)?.scrollIntoView({ behavior: "smooth" });
                   setActiveSection(section);
                 }}
-                whileHover={{ scale: 1.1 }}
+                whileHover={{ scale: 1.05 }}
                 transition={{ duration: 0.3 }}
-                className={`transition duration-300 text-sm md:text-xl px-4 py-2 md:px-6 md:py-3 rounded-full flex items-center 
-                    font-medium tracking-wide capitalize
-                    ${
-                      activeSection === section
-                        ? "bg-orange-400 text-gray-900 font-bold shadow-md shadow-orange-500"
-                        : "hover:text-orange-400 hover:bg-gray-700 hover:bg-opacity-40"
+                className={`transition duration-300 text-sm md:text-lg px-4 py-2 md:px-6 md:py-3 rounded-full flex items-center 
+                    font-medium tracking-wide capitalize ${activeSection === section
+                      ? "bg-orange-400 text-gray-900 font-bold shadow-md shadow-orange-500"
+                      : "hover:text-orange-400 hover:bg-gray-700 hover:bg-opacity-40"
                     }`}
               >
                 {section.charAt(0).toUpperCase() + section.slice(1)}
               </motion.a>
-            )
-          )}
+            ))}
+          </div>
         </div>
       </motion.nav>
 
@@ -126,42 +117,36 @@ const navVariants = {
               />
             </div>
 
-            {["home", "about", "projects", "experiences", "contact"].map(
-              (section) => (
-                <motion.a
-                  key={section}
-                  href={`#${section}`}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    document
-                      .getElementById(section)
-                      ?.scrollIntoView({ behavior: "smooth" });
-                    setShowNav(false);
-                    setActiveSection(section);
-                  }}
-                  whileHover={{ scale: 1.1 }}
-                  transition={{ duration: 0.3 }}
-                  className={`text-xl font-medium capitalize px-6 py-3 rounded-md w-full text-center
-                    ${
-                      activeSection === section
-                        ? "bg-orange-400 text-gray-900 font-bold"
-                        : "hover:text-orange-400 hover:bg-gray-700"
+            {["home", "about", "projects", "experiences", "contact"].map((section) => (
+              <motion.a
+                key={section}
+                href={`#${section}`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  document.getElementById(section)?.scrollIntoView({ behavior: "smooth" });
+                  setShowNav(false);
+                  setActiveSection(section);
+                }}
+                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.3 }}
+                className={`text-xl font-medium capitalize px-6 py-3 rounded-md w-full text-center
+                    ${activeSection === section
+                      ? "bg-orange-400 text-gray-900 font-bold"
+                      : "hover:text-orange-400 hover:bg-gray-700"
                     }`}
-                >
-                  {section.charAt(0).toUpperCase() + section.slice(1)}
-                </motion.a>
-              )
-            )}
+              >
+                {section.charAt(0).toUpperCase() + section.slice(1)}
+              </motion.a>
+            ))}
           </motion.nav>
         )}
       </AnimatePresence>
-    
 
       {/* HERO SECTION */}
-      <Hero/>
+      <Hero />
 
       {/* ABOUT SECTION */}
-      <About/>
+      <About />
 
       {/* PROJECTS SECTION */}
       <Project />
@@ -171,7 +156,6 @@ const navVariants = {
 
       {/* CONTACT SECTION */}
       <Contact />
-
     </div>
   );
 }
